fbbc09041eecf335bd7f824a7beb4bcc8fbb84fa
kazasaki23311 <julia.nikashina@gmail.com>
2026-02-28 22:05:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,9 @@
 
 bool checkPrime(uint64_t value) {
   for (uint64_t i = 2; i <= pow(value, 0.5); i++) {
-    if ((value % i) == 0) return false;
+    if ((value % i) == 0) {
+      return false;
+    }
   }
   return true;
 }
@@ -14,8 +16,10 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
   uint64_t check = 0, i = 1;
   while (check != n) {
-	  i++;
-	  if (checkPrime(i)) check++;
+    i++;
+    if (checkPrime(i)) {
+      check++;
+    }
   }
   return i;
 }
@@ -23,14 +27,16 @@ uint64_t nPrime(uint64_t n) {
 uint64_t nextPrime(uint64_t value) {
   uint64_t i = value + 1;
   while (!checkPrime(i))
-	  i++;
+    i++;
   return i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t summa = 0;
   for (uint64_t i = 2; i < hbound; i++) {
-	  if (checkPrime(i)) summa += i;
+    if (checkPrime(i)) {
+      summa += i;
+    }
   }
   return summa;
 }
@@ -38,9 +44,9 @@ uint64_t sumPrime(uint64_t hbound) {
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
   uint64_t kolvo = 0, i;
   for (i = lbound; i < hbound; i++) {
-	  if (checkPrime(i) && checkPrime(i + 2)) {
-		  kolvo++;
-	  }
+    if (checkPrime(i) && checkPrime(i + 2)) {
+      kolvo++;
+    }
   }
   return kolvo;
 }
